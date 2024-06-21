e369fa98a5e3f42006716be9c7b0eb6a62252a06
lubin <binidentity@gmail.com>
2024-06-21 15:10:27 +0800

    remove unused

--- fstab.c
+++ fstab.c
@@ -1,18 +1,11 @@
-#include <ultracore.h>
-#include <unistd.h>
 #include <flash.h>
 #include <fs/ultrafs.h>
-#include <i2c.h>
 
 /****************************************************************************
  *  @filesystem
  ****************************************************************************/
-void FILESYSTEM_startup(void) /** @override */
+void FILESYSTEM_startup(void)
 {
-    /*
-    int flash_fd = FLASH_createfd();
-
-    if (0 == ULTRAFS_mount(flash_fd, "flashfs", FLASH_PAGE_SIZE, FLASH_AVAIL_SIZE, FLASH_ERASE_FILL))
-        chdir("/mnt/flashfs");
-    */
+    // int flash_fd = FLASH_createfd();
+    // ULTRAFS_mount_root(flash_fd, FLASH_PAGE_SIZE, FLASH_AVAIL_SIZE, FLASH_ERASE_FILL);
 }
