f937c99227ea010bb30747b81f2eb28113e0eb51
lubin <binarylu@hotmail.com>
2024-09-20 16:42:52 +0800

    version inc

--- talking_button/config/PERIPHERAL_config.h
+++ talking_button/config/PERIPHERAL_config.h
@@ -4,7 +4,7 @@
 #include <gpio.h>
 
     #define PROJECT_NAME                "smartcuckoo"
-    #define PROJECT_VERSION             VERSION_INFO(1, 0, 19)
+    #define PROJECT_VERSION             VERSION_INFO(1, 0, 20)
     #define PROJECT_ID                  "tbtn"
 
 // buttons
