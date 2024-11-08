50516a27733c4c325e5dd6c2821bb0ee17fe5a06
lubin <binidentity@gmail.com>
2024-11-08 17:49:31 +0800

    override to prevent link filesystem

--- fstab.c
+++ fstab.c
@@ -1,11 +1,11 @@
 #include <flash.h>
 #include <fs/ultrafs.h>
 
-/****************************************************************************
- *  @filesystem
- ****************************************************************************/
-void FILESYSTEM_startup(void)
+void FILESYSTEM_initialize(void)
 {
-    // int flash_fd = FLASH_createfd();
-    // ULTRAFS_mount_root(flash_fd, FLASH_PAGE_SIZE, FLASH_AVAIL_SIZE, FLASH_ERASE_FILL);
+}
+
+void FILESYSTEM_fd_cleanup(int fd)
+{
+    ARG_UNUSED(fd);
 }
