7de5f1233924549d897bd8f4fc55a1c3409dda02
lubin <binidentity@gmail.com>
2024-11-13 15:18:01 +0800

    update retain RTC ts

--- talking_button/talking_button.c
+++ talking_button/talking_button.c
@@ -164,7 +164,7 @@ void PERIPHERAL_on_sleep(void)
 
 void PERIPHERAL_on_wakeup(void)
 {
-    // printf("\n");
+    BURAM->RET[31].REG = BURTC->CNT;    // RTC
     mqueue_postv(talking_button.mqd, MSG_ALIVE, 0, 0);
 }
 
@@ -584,7 +584,6 @@ static __attribute__((noreturn)) void *MSG_dispatch_thread(struct talking_button
         if (msg)
         {
             WDOG_feed();
-            BURAM->RET[31].REG = BURTC->CNT;    // RTC
 
             switch ((enum talking_button_message_t)msg->msgid)
             {
