5a95594c422809ec558654e1b47827a139d5553c
lubin <binidentity@gmail.com>
2024-12-03 16:06:03 +0800

    LOG_print => LOG_printf

--- mplayer_voice.c
+++ mplayer_voice.c
@@ -1055,7 +1055,7 @@ int VOICE_say_time(struct VOICE_attr_t *attr, struct tm const *tm)
     if (NULL == attr->voice)
         return EMODU_NOT_CONFIGURED;
 
-    LOG_print("%04d/%02d/%02d %02d:%02d:%02d",
+    LOG_printf("%04d/%02d/%02d %02d:%02d:%02d",
         tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
         tm->tm_hour, tm->tm_min, tm->tm_sec);
 
