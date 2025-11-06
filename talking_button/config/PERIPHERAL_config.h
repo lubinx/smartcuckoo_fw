4d37014e4dbb63ec7b4302ea184c3ebf90b8047c
lubin <binidentity@gmail.com>
2025-11-06 19:24:50 +0800

    1.0.36 sd card compatiable

--- talking_button/config/PERIPHERAL_config.h
+++ talking_button/config/PERIPHERAL_config.h
@@ -5,7 +5,7 @@
 #include <gpio.h>
 
     #define PROJECT_NAME                "smartcuckoo"
-    #define PROJECT_VERSION             VERSION_INFO(1, 0, 35)
+    #define PROJECT_VERSION             VERSION_INFO(1, 0, 36)
     #define PROJECT_ID                  "tbtn"
 
 // console
